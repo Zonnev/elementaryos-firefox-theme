00b8da2f82329813d8af4b4a78835a6f0c33273c
Zonnev <mikolov73@gmail.com>
2020-11-05 08:44:34 +0100

    Added Odin files

---
+++ install.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+set -e
+
+function selectLayout() {
+	
+	if [ "${1}" != "" ]; then
+		USER_CHROME_LAYOUT=${1}
+	else
+		echo "Select Available Layouts:"
+		echo ""
+		echo "   1. Default"
+		echo "   2. Close Only Left"
+		echo "   3. Close Only Right"
+		echo "   4. Minimize Left"
+		echo "   5. Minimize Right"
+		echo "   6. OSX"
+		echo "   7. Ubuntu"
+		echo "   8. Windows"
+		echo ""
+		read -p "Please select the preferred layout (press Enter for default) [1-8]:" USER_CHROME_LAYOUT < /dev/tty
+		echo ""
+	fi
+	
+	case $USER_CHROME_LAYOUT in
+		2 )
+		    echo "Calling the install script for the 'Close Only Left' layout"
+			curl -s -o- https://raw.githubusercontent.com/Zonnev/elementaryos-firefox-theme/elementaryos-odin-firefox-theme/Close%20Only%20Left/install.sh | bash
+			exit 0
+			;;
+		3 )
+		    echo "Calling the install script for the 'Close Only Right' layout"
+			curl -s -o- https://raw.githubusercontent.com/Zonnev/elementaryos-firefox-theme/elementaryos-odin-firefox-theme/Close%20Only%20Right/install.sh | bash
+			exit 0
+			;;
+		4 )
+		    echo "Calling the install script for the 'Minimize Left' layout"
+			curl -s -o- https://raw.githubusercontent.com/Zonnev/elementaryos-firefox-theme/elementaryos-odin-firefox-theme/Minimize%20Left/install.sh | bash
+			exit 0
+			;;
+		5 )
+		    echo "Calling the install script for the 'Minimize Right' layout"
+			curl -s -o- https://raw.githubusercontent.com/Zonnev/elementaryos-firefox-theme/elementaryos-odin-firefox-theme/Minimize%20Right/install.sh | bash
+			exit 0
+			;;
+		6 )
+		    echo "Calling the install script for the 'OSX' layout"
+			curl -s -o- https://raw.githubusercontent.com/Zonnev/elementaryos-firefox-theme/elementaryos-odin-firefox-theme/OSX/install.sh | bash
+			exit 0
+			;;
+		7 )
+		    echo "Calling the install script for the 'Ubuntu' layout"
+			curl -s -o- https://raw.githubusercontent.com/Zonnev/elementaryos-firefox-theme/elementaryos-odin-firefox-theme/Ubuntu/install.sh | bash
+			exit 0
+			;;
+		8 )
+		    echo "Calling the install script for the 'Windows' layout"
+			curl -s -o- https://raw.githubusercontent.com/Zonnev/elementaryos-firefox-theme/elementaryos-odin-firefox-theme/Windows/install.sh | bash
+			exit 0
+			;;
+		* )
+		    echo "Installing Default layout"
+			;;
+	esac
+	echo ""
+}
+
+function saveProfile() {
+	ITEM="${1}"
+	PROFILE_PATH="${2}"
+	PROFILE_CHROME_DIR="${FIREFOX_DIR}/${PROFILE_PATH}/chrome"
+    PROFILE_USER_CHROME_CSS_FILE="${PROFILE_CHROME_DIR}/userChrome.css"
+    PROFILE_USER_CONTENT_CSS_FILE="${PROFILE_CHROME_DIR}/userContent.css"
+    echo "  $((${ITEM}+3)). Install theme at profile path ${FIREFOX_DIR}/${PROFILE_PATH}:"
+    echo -n "     - Ensure directory 'chrome' ... "
+    mkdir -p "${PROFILE_CHROME_DIR}"
+    echo "done"
+    echo -n "     - Create file 'chrome/userChrome.css' ... "
+    curl -s -o "${PROFILE_USER_CHROME_CSS_FILE}" "${USER_CHROME_CSS_URL}" -s -o "${PROFILE_USER_CONTENT_CSS_FILE}" "${USER_CONTENT_CSS_URL}"
+    echo "done"
+}
+
+echo "Install ElementaryOS Odin Firefox Theme (https://github.com/Zonnev/elementaryos-firefox-theme/tree/elementaryos-odin-firefox-theme)"
+echo ""
+
+selectLayout "$1"
+
+FIREFOX_DIR="${HOME}/.mozilla/firefox"
+FIREFOX_DIR_FLATPAK="${HOME}/.var/app/org.mozilla.firefox/.mozilla/firefox"
+USER_CHROME_CSS_URL="https://raw.githubusercontent.com/Zonnev/elementaryos-firefox-theme/elementaryos-odin-firefox-theme/userChrome.css"
+USER_CONTENT_CSS_URL="https://raw.githubusercontent.com/Zonnev/elementaryos-firefox-theme/elementaryos-odin-firefox-theme/userContent.css"
+
+echo -n "  1. Check Firefox installation ... "
+if [ ! -d "${FIREFOX_DIR}" ]; then
+	if [ ! -d "${FIREFOX_DIR_FLATPAK}" ]; then
+		>&2 echo "failed, please check Firefox installation, unable to find ${FIREFOX_DIR} or ${FIREFOX_DIR_FLATPAK}"
+		exit 1
+	else
+		FIREFOX_DIR="${FIREFOX_DIR_FLATPAK}"
+	fi
+fi
+PROFILES_FILE="${FIREFOX_DIR}/profiles.ini"
+if [ ! -f "${PROFILES_FILE}" ]; then
+	>&2 echo "failed, please check Firefox installation, unable to find profile.ini at ${FIREFOX_DIR}"
+	exit 1
+fi
+echo " done"
+	
+echo -n "  2. Search for firefox profiles ... "
+PROFILES_PATHS=($(grep -E "^Path=" "${PROFILES_FILE}" | cut -d "=" -f2-))
+if [ ${#PROFILES_PATHS[@]} -eq 0 ]; then
+	>&2 echo "failed, no profiles found at ${PROFILES_FILE}"
+	exit 0
+elif [ ${#PROFILES_PATHS[@]} -eq 1 ]; then
+	echo "one profile found"
+	saveProfile "0" "${PROFILES_PATHS[0]}"
+else
+	echo "${#PROFILES_PATHS[@]} profiles found"
+	ITEM=0
+	for PROFILE_PATH in "${PROFILES_PATHS[@]}"; do
+	    saveProfile "${ITEM}" "${PROFILE_PATH}"
+	    ITEM="$((${ITEM}+1))"
+	done
+fi
+
+echo ""
+echo "Done, restart Firefox, please."
---
+++ readme.md
@@ -0,0 +1,31 @@
+# An elementary OS 5 (Hera) theme for Firefox
+![Screenshot](theme.png)
+
+## Credits
+
+Credits to [Harvey Cabaguio](https://github.com/harveycabaguio/firefox-elementary-theme) for setting the theme up, [Surendrajat](https://github.com/Surendrajat), [DRHAX34](https://github.com/DRHAX34) and [sempasha](https://github.com/sempasha) for the install script and the elementary team for the UI design and icons.
+
+## Install
+
+**Firefox does not support userChrome.css by default. Here are the steps to make it work:**
+
+  1. Load **about:config** in the Firefox address bar.
+  2. Confirm that you will be careful.
+  3. Search for `toolkit.legacyUserProfileCustomizations.stylesheets` using the search at the top.
+  4. Toggle the preference by double clicking. True means Firefox supports the CSS files, False that it ignores them.
+
+**Follow this step to install the userChrome.css:**
+
+You can set 8 different window buttons layouts with [Pantheon Tweaks](https://github.com/pantheon-tweaks/pantheon-tweaks/). Now it's possible to install this theme to match these window button layouts.
+
+Use this one line install script which let you select the window buttons layout you like:
+
+```bash
+curl -s -o- https://raw.githubusercontent.com/Zonnev/elementaryos-firefox-theme/elementaryos-odin-firefox-theme/install.sh | bash
+```
+Just copy the line to your terminal and press enter. Then it lets you choose which window buttons layout you need (1-8) and press enter again. If you don't want to install elementary Tweaks, then you select the default layout (option 1 in the installer).
+
+## After installation
+
+1. Disable Title bar in the customization panel in Firefox. 
+2. In the customization panel in Firefox you can move the new tab button to the left if you wish and use the dark mode option as well, in combination with [Pantheon Tweaks](https://github.com/pantheon-tweaks/pantheon-tweaks/).
